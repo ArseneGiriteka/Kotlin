e7582e3928af06338226b55445d913ea431c89c6
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-09-22 07:43:06 +0200

    Inheritance

--- Animals/src/Animals.kt
+++ Animals/src/Animals.kt
@@ -20,3 +20,59 @@ open class Animal {
         println("The animal is sleeping")
     }
 }
+
+class Hippo: Animal() {
+    override val image = "Hippo.jpg"
+    override val food = "Grass"
+    override val habitat = "Water"
+
+    override fun makeNoise() {
+        println("Grunt! Grunt!")
+    }
+
+    override fun eat() {
+        println("The Hippo is eating $food")
+    }
+}
+
+open class Canine: Animal() {
+    override fun roam() {
+        println("Canine is roaming")
+    }
+}
+
+class Wolf: Canine() {
+    override val image = "wolf.gpj"
+    override val food = "meat"
+    override val habitat = "Forest"
+
+    override fun makeNoise() {
+        println("Hoooooow!")
+    }
+
+    override fun eat() {
+        println("Wolf is eating $food")
+    }
+}
+
+class Vet {
+    fun giveShot(animal: Animal) {
+        animal.makeNoise()
+    }
+}
+
+fun main(args: Array<String>) {
+    val animals = arrayOf(Hippo(), Wolf())
+
+    for (animal in animals) {
+        animal.roam()
+        animal.eat()
+    }
+
+    val vet = Vet()
+    val wolf = Wolf()
+    val hippo = Hippo()
+
+    vet.giveShot(wolf)
+    vet.giveShot(hippo)
+}
