f286999dbb3add48095f4e5887b151d840ba0591
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-09-22 09:36:44 +0200

    Add interface Roamable

--- Animals/src/Animals.kt
+++ Animals/src/Animals.kt
@@ -1,4 +1,8 @@
-abstract class Animal {
+interface Roamable {
+    fun roam()
+}
+
+abstract class Animal: Roamable {
     abstract val image: String
     abstract val food: String
     abstract val habitat: String
@@ -8,7 +12,7 @@ abstract class Animal {
 
     abstract fun eat()
 
-    open fun roam() {
+    override fun roam() {
         println("The animal is roaming")
     }
 
@@ -48,6 +52,12 @@ class Wolf: Canine() {
 
     override fun eat() {
         println("Wolf is eating $food")
+    }
+}
+
+class Vehicle: Roamable {
+    override fun roam() {
+        println("The vehicle is roaming")
     }
 }
 
